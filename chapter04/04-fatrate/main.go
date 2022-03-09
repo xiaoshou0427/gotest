63191ec2ad36f20f65d10deca933cc222d9892c9
Oscar <xiaoshou0427@foxmail.com>
2022-03-10 01:39:47 +0800

    add pointer explanation

--- chapter04/04-fatrate/main.go
+++ chapter04/04-fatrate/main.go
@@ -64,3 +64,17 @@ func getFakePersonInfo() *Person {
 		age:    35,
 	}
 }
+//下面的修改案例更好说明，上面是简写的，预期产出是一个指针类型的结构体（也可以叫指针变量吧？），return 返回真实的值给到预期
+// xyz 为指针变量（是指针类型），指向结构体person{小强 男 1.7 70 35 24.221453287197235 0.20565743944636683}的内存地址
+//给到person := getFakePersonInfo() 这个调用函数，直接将person 设置为指针类型的变量！
+//值为结构体erson{小强 男 1.7 70 35 24.221453287197235 0.20565743944636683}的内存地址
+//func getFakePersonInfo() *Person {
+//	xyz := &Person{
+//		name:   "小强",
+//		sex:    "男",
+//		tall:   1.7,
+//		weight: 70,
+//		age:    35,
+//	}
+//	return xyz
+//}
